7f635b4ade5a37e751064622f5d63c76ea38ab92
benxi809 <2226384637@qq.com>
2026-06-10 21:52:57 +0800

    fix: repair corrupted MainActivity.kt (was damaged during pump code cleanup)

--- android/app/src/main/kotlin/com/glucare/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/glucare/app/MainActivity.kt
@@ -1,6 +1,5 @@
-     1|package com.glucare.app
-     2|
-     3|import io.flutter.embedding.android.FlutterActivity
-     4|
-     5|class MainActivity : FlutterActivity()
-     6|
+package com.glucare.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
